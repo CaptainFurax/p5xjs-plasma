fae66184ddc4a2129fac006196bd8a5e7de2f68b
CaptnFurax <100019725+CaptainFurax@users.noreply.github.com>
2022-05-02 09:32:08 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+setup=_=>{
+  x=64,y=48,d=128
+  createCanvas(x*8,y*8)
+  strokeWeight(1)}
+draw=_=>{
+  for(j=y;j--;){
+    for(i=x;i--;){
+      c=sin(noise(i/d,j/d,frameCount/d)*d)
+      r=map(c,-1,1,63,255)
+      b=map(c,-1,1,255,63)
+      fill(color(r,r,b))
+      rect(i*8,j*8,8,8)}}}
