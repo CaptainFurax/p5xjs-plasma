993b5a96ea2b0fcba7ca7e81b006b9fb97fc9909
CaptnFurax <100019725+CaptainFurax@users.noreply.github.com>
2022-05-04 08:49:13 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
 setup=_=>{
   x=64,y=48,d=128
   createCanvas(x*8,y*8)
-  strokeWeight(1)}
+  strokeWeight(1)
+}
 draw=_=>{
   for(j=y;j--;){
     for(i=x;i--;){
@@ -9,4 +10,7 @@ draw=_=>{
       r=map(c,-1,1,63,255)
       b=map(c,-1,1,255,63)
       fill(color(r,r,b))
-      rect(i*8,j*8,8,8)}}}
+      rect(i*8,j*8,8,8)
+    }
+  }
+}
